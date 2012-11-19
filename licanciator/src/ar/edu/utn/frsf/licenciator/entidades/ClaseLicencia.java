d0ad942d0c48d1778252824329b68024751df046
Las Autenticas Aguilas <cotii.q@gmail.com>
2012-11-19 17:03:13 -0300

    Modificados Test de Emitir Logica Y algo mas...

--- licanciator/src/ar/edu/utn/frsf/licenciator/entidades/ClaseLicencia.java
+++ licanciator/src/ar/edu/utn/frsf/licenciator/entidades/ClaseLicencia.java
@@ -10,20 +10,21 @@ public class ClaseLicencia {
 	private String tipo;
 	private String descripcion;
 	private int edadMinima;
-	private int[] costo = new int[10];
+	
+	/* Al no estar implementado el "Calcular costo vigencia, se toma el mismo
+	 * costo para toda la clase.
+	 */
+	private int costo;
 	
 	public ClaseLicencia ()
 	{}
 
-	public ClaseLicencia (String tipo, String desc, int edad, int c1, int c2, int c3, int c4)
+	public ClaseLicencia (String tipo, String desc, int edad, int c1)
 	{
 		this.tipo = tipo;
 		descripcion = desc;
 		edadMinima = edad;
-		costo[0] = c1;
-		costo[1] = c2;
-		costo[2] = c3;
-		costo[3] = c4;
+		costo = c1;
 	}
 	
 	public String getTipo()
@@ -41,13 +42,13 @@ public class ClaseLicencia {
 		return edadMinima;
 	}
 	
-	public int[] getCosto()
+	public int getCosto()
 	{
 		return costo;
 	}
 	
 	public boolean equals(ClaseLicencia clas)
 	{
-		return (tipo.equals(clas.tipo) && descripcion.equalsIgnoreCase(clas.descripcion) && edadMinima==clas.edadMinima && costo[0]==clas.costo[0] && costo[1]==clas.costo[1] && costo[2]==clas.costo[2] && costo[3]==clas.costo[3]);
+		return (tipo.equals(clas.tipo) && descripcion.equalsIgnoreCase(clas.descripcion) && edadMinima==clas.edadMinima && costo==clas.costo);
 	}
 }
